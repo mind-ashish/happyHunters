58f7f63a26f155611dd6ce3e2ddfe192bb2fe058
Aashish Choudhary <aashish01official@gmail.com>
2021-07-05 22:48:35 +0530

    angular app

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 var myApp=angular
             .module("myModule",[])
             .controller("myController",function($scope, $rootScope){
-                alert("hello");
-            });
+                $scope.job = {};
+        $scope.submitForm = function() {
+        $http({
+          method  : 'POST',
+          url     : '/api/v1/jobs/createJob',
+          data    : $scope.job,
+          headers : { 'Content-Type': 'application/x-www-form-urlencoded' } 
+         })
+          .success(function(data) {
+            if (data.errors) {
+              alert('error in creating job');
+            } else {
+              alert('Job created successfully');
+            }
+          });
+        };
+     });
