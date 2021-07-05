d3e75ec5770a46e0fdf6e1d955677b79d3d5d5f5
Aashish Choudhary <aashish01official@gmail.com>
2021-07-05 10:19:20 +0530

    api

---
+++ routes/api/v1/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use('/colleges', require('./colleges'));
+router.use('/jobs', require('./jobs'));
+router.use('/candidates', require('./candidates'));
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,5 @@ const homeController=require('../controllers/home_controller');
 router.get('/',homeController.home);
 router.use('/recruiter',require('./recruiter'));
 router.use('/candidate',require('./candidate'));
+router.use('/api',require('./api'));
 module.exports=router;
